93388b1f65ae78a3c0b60e4ab5a497e82947660e
nwtls <neverwhatlose@mail.ru>
2024-07-17 02:32:44 +0300

    Измененен принцип регистрации команды, добавлены необязательные аргументы bound (радиус rtp), type (от игрока 'player' или от нулевых координат 'zero')

--- src/main/java/ru/nwtls/simplertpplugin/SimpleRtpPlugin.java
+++ src/main/java/ru/nwtls/simplertpplugin/SimpleRtpPlugin.java
@@ -1,18 +1,46 @@
 package ru.nwtls.simplertpplugin;
 
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.paper.PaperCommandManager;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+//import org.incendo.cloud.execution.ExecutionCoordinator;
+//import org.incendo.cloud.paper.LegacyPaperCommandManager;
+//import org.incendo.cloud.paper.PaperCommandManager;
 import ru.nwtls.simplertpplugin.commands.RandomTeleportCommand;
 
-public final class SimpleRtpPlugin extends JavaPlugin {
+import java.util.function.Function;
 
+public final class SimpleRtpPlugin extends JavaPlugin {
+    PaperCommandManager<CommandSender> commandManager;
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getCommand("rtp").setExecutor(new RandomTeleportCommand());
+
+//        PaperCommandManager<CommandSourceStack> commandManager = PaperCommandManager.builder()
+//                .executionCoordinator(ExecutionCoordinator.simpleCoordinator())
+//                .buildOnEnable(this);
+        try {
+            commandManager = new PaperCommandManager<>(
+                    /* Owning plugin */ this,
+                    /* (1) */ CommandExecutionCoordinator.simpleCoordinator(),
+                    /* (2) */ Function.identity(),
+                    Function.identity()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        new RandomTeleportCommand(commandManager);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+//    public static SimpleRtpPlugin getInstance() {
+//        return SimpleRtpPlugin.getPlugin(SimpleRtpPlugin.class);
+//    }
 }
--- src/main/java/ru/nwtls/simplertpplugin/commands/RandomTeleportCommand.java
+++ src/main/java/ru/nwtls/simplertpplugin/commands/RandomTeleportCommand.java
@@ -1,15 +1,15 @@
 package ru.nwtls.simplertpplugin.commands;
 
+import cloud.commandframework.arguments.standard.IntegerArgument;
+import cloud.commandframework.arguments.standard.StringArgument;
+import cloud.commandframework.paper.PaperCommandManager;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
-import org.bukkit.Server;
 import org.bukkit.World;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitScheduler;
+//import org.incendo.cloud.paper.PaperCommandManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
@@ -17,41 +17,71 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class RandomTeleportCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if(!(sender instanceof Player)) {
-            sender.sendMessage("Only players can execute this command.");
-            return true;
-        }
-        Player player = (Player) sender;
+public class RandomTeleportCommand {
+
+    public RandomTeleportCommand(@NotNull PaperCommandManager<CommandSender> manager) {
+        manager.command(manager
+                .commandBuilder("rtp", "randomteleport", "randtp")
+                .argument(IntegerArgument.<CommandSender>builder("bound").asOptional())
+                .argument(StringArgument.<CommandSender>builder("type").asOptional())
+                .handler(ctx -> {
+                    if (!(ctx.getSender() instanceof Player)) {
+                        ctx.getSender().sendMessage("You must be a player to use this command.");
+                        return;
+                    }
+                    Integer bound = ctx.getOrDefault("bound", 5000);
+                    String type = ctx.getOrDefault("type", "player");
+                    if (!(type.equals("player") || type.equals("zero"))) {
+                        ctx.getSender().sendMessage("Type must be either player or zero");
+                        return;
+                    }
+                    this.handle((Player) ctx.getSender(), bound, type);
+                }
+        ));
+    }
+
+    public void handle(@NotNull Player player, Integer bound, @NotNull String type) {
         World world = player.getWorld();
 
-        // Идею с таймером придется отложить...
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int i = 3;
+            @Override
+            public void run() {
+                Component msg = Component.text()
+                        .append(Component.text("Teleporting in " + i + " seconds"))
+                        .color(NamedTextColor.GRAY)
+                        .build();
+                player.sendMessage(msg);
+                i--;
+                if (i == 0) {
+                    Random rand = new Random();
 
-//        Timer timer = new Timer();
-//        timer.schedule(new TimerTask() {
-//            int i = 3;
-//            @Override
-//            public void run() {
-//                Component msg = Component.text()
-//                        .append(Component.text("Teleporting in " + i + " seconds"))
-//                        .color(NamedTextColor.GRAY)
-//                        .build();
-//                player.sendMessage(msg);
-//                i--;
-//                if (i == 0) {
-//                    this.cancel();
-//                }
-//            }
-//        }, 0, 1000);
+                    int curX = player.getLocation().getBlockX();
+                    int curZ = player.getLocation().getBlockZ();
+                    int x;
+                    int z;
+                    String message;
 
-        Random rand = new Random();
-        int x = rand.nextInt(5000);
-        int z = rand.nextInt(5000);
-        int y = world.getHighestBlockYAt(x, z);
-        Location loc = new Location(world, x, y, z);
-        player.teleport(loc);
-        return true;
+                    if (type.equals("player")) {
+                        x = curX + 2 * rand.nextInt(bound) - bound;
+                        z = curZ + 2 * rand.nextInt(bound) - bound;
+                        message = "Changed x: " + (x - curX)  + ", z: " + (z - curX) + ", type: " + type;
+                    } else {
+                        x = 2 * rand.nextInt(bound) - bound;
+                        z = 2 * rand.nextInt(bound) - bound;
+                        message = "Changed x: " + (x - curX)  + ", z: " + (z - curX) + ", type: " + type;
+                    }
+
+                    int y = world.getHighestBlockYAt(x, z);
+                    Location loc = new Location(world, x, y, z);
+                    player.teleportAsync(loc);
+
+                    player.sendMessage(message);
+                    this.cancel();
+                }
+            }
+        }, 0, 1000);
     }
 }
+
